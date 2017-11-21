c0f865381454a9fdbf0c9b290db0d23650f666e3
hadilelrody <Elrody52@gmail.com>
2017-11-21 15:06:52 -0800

    Added All Courses Function
    3ash ya haaddiiiiilllllll

--- Course Prerequsites WPF/Classes/Course.cs
+++ Course Prerequsites WPF/Classes/Course.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace Course_Prerequsites_WPF.Classes
 {
@@ -18,6 +20,7 @@ namespace Course_Prerequsites_WPF.Classes
         public string Description { get; set; }
         public List<string> PreRequiredCourses { get; set; }
 
+
         Course(string code, string coursename, string Maximumnumber, string passinggrade, string coursegrade, string hours, string instructor, string description, List<string> pre)
         {
             Code = code;
@@ -28,11 +31,53 @@ namespace Course_Prerequsites_WPF.Classes
             Hours = hours;
             Instructor = instructor;
             Description = description;
-            PreRequiredCourses = pre; 
+            PreRequiredCourses = pre;
         }
 
 
+        public List<Course> AllCourses()
+        {
+            FileStream fs = new FileStream("ReadingTrial.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
 
+            string[] fileds;
+            string[] records;
+            string[] prerequirds;
+
+            List<string> pr = new List<string>();
+            List<Course> l = new List<Course>();
+
+            while (sr.Peek() != -1)
+            {
+                records = sr.ReadLine().Split('#');
+
+                for (int i = 0; i < records.Length - 1; i++)
+                {
+                    fileds = records[i].Split('%');
+
+                    Code = fileds[0];
+                    CourseName = fileds[1];
+                    MaximumNumberOfStudents = fileds[2];
+                    PassingGrade = fileds[3];
+                    CourseGrade = fileds[4];
+                    Hours = fileds[5];
+                    Instructor = fileds[6];
+                    Description = fileds[7];
+                    prerequirds = fileds[8].Split('*');
+
+                    for (int j = 0; j < prerequirds.Length; j++)
+                    {
+                        pr.Add(prerequirds[j]);
+                    }
+                }
+
+                Course c = new Course(Code, CourseName, MaximumNumberOfStudents, PassingGrade, CourseGrade, Hours, Instructor, Description, PreRequiredCourses);
+                l.Add(c);
+            }
+
+            return l;
+
+        }
 
 
     }
