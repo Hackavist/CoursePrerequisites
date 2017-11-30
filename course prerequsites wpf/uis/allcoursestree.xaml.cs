af1f8dca1dc6948ddac49d008c3ff4da0c74dfe8
Nour ElGafy <nour-elgafy@outlook.com>
2017-11-30 15:30:25 +0200

    Cleaned the tree code

--- course prerequsites wpf/uis/allcoursestree.xaml.cs
+++ course prerequsites wpf/uis/allcoursestree.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.IO;
 using Course_Prerequsites_WPF.Classes;
 
 namespace Course_Prerequsites_WPF.UIs
@@ -29,62 +19,65 @@ namespace Course_Prerequsites_WPF.UIs
         //tree Parents Appear On Load
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            /*
-            // get every local drive on the machine 
-            foreach(var item in Directory.GetLogicalDrives() )
-            {
-                //create a tree view item for it 
-                var branch = new TreeViewItem();
-
-                //set the header 
-                branch.Header = item;
-
-                //set the path 
-                branch.Tag = item;
-
-                //listen out for items being expanded 
-
-                branch.Expanded += Branch_Expanded;
-
-                AllCourses.Items.Add(branch);
-                branch.Items.Add(null);
-            }*/
-
-            Dictionary<string,Course> AllCoursesObj = new Dictionary<string, Course>();
+            //returns a dictionary full of all courses 
+            Dictionary<string, Course> AllCoursesObj = new Dictionary<string, Course>();
             Course cour = new Course();
+            //dictionary of all courses
             AllCoursesObj = cour.GetAllCourses();
 
+            //for each course in the dictionary create and  add a tree view item 
             foreach (var course in AllCoursesObj)
             {
-                var branch = new TreeViewItem();
+                var branch = new TreeViewItem(); // new treeview item 
 
-                branch.Header = course.Value.CourseName;
+                branch.Header = course.Value.CourseName; // sets the text header of the tree view item to the course Name
 
-                AllCourses.Items.Add(branch);
+                branch.HeaderStringFormat = course.Value.CourseName;
 
-                foreach (var pre in course.Value.PreRequiredCourses)
-                {
-                    var leaf = new TreeViewItem();
-                    var l = course.Value.ReturnObj(pre);
-                    branch.Items.Add(l);
-                }
+                branch.Items.Add(null); // addes a node to the tree 
+
+                branch.Expanded += Branch_Expanded; // what happens when the tree is expanded 
+
+                AllCourses.Items.Add(branch); // addes the branch to the parent 
 
             }
         }
 
         private void Branch_Expanded(object sender, RoutedEventArgs e)
         {
-            var item = (TreeViewItem)sender;
-            //checks if the containing are dummy data  
-            if (item.Items.Count != 1 || item.Items[0] != null)
+            //parces the sender object into a mai
+            var x = (TreeViewItem)sender;
+
+            // check if there is a null 
+            if (x.Items.Count != 1 || x.Items[0] != null)
             {
                 return;
             }
 
-            //Cleans Dummy Data 
-            item.Items.Clear();
+            //cleans the nulls  
+            x.Items.Clear();
 
+            //extracts the name of the subject from the sender 
+            string name = x.HeaderStringFormat;
 
+            // creates an object of the given name 
+            Course cour = new Course();
+            cour= cour.ReturnObj(name);
+
+            //Reveses the list to out the prequisists in the correct order 
+            cour.PreRequiredCourses.Reverse();
+
+            // loops on the list of prequesists  
+            foreach (var pre in cour.PreRequiredCourses)
+            {
+                // creates a new sub element of the parent 
+                var subitem = new TreeViewItem();
+                //sets the text of the sub element to the text of the prequisists  
+                subitem.Header = pre;
+                //addes the sub item to parent 
+                x.Items.Add(subitem);
+
+            }
         }
     }
 }
