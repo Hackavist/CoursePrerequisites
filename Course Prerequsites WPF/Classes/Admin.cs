27fec451335bec190695470509eb175af48a28b0
hazemkhairy <hazemkhairy188@gmail.com>
2017-11-30 09:32:27 +0200

    admin class

---
+++ Course Prerequsites WPF/Classes/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_Prerequsites_WPF.Classes
+{
+    class Admin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool GM { get; set; }
+
+    }
+}
