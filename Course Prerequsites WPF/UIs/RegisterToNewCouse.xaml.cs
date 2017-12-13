f6798c3a181b8876b6d9f595f2b02361536bac4d
hadilelrody <Elrody52@gmail.com>
2017-12-12 20:21:55 -0800

    Added Function in student

---
+++ Course Prerequsites WPF/UIs/RegisterToNewCouse.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Course_Prerequsites_WPF.Classes;
+using Course_Prerequsites_WPF.UIs;
+
+namespace Course_Prerequsites_WPF.UIs
+{
+    /// <summary>
+    /// Interaction logic for RegisterToNewCouse.xaml
+    /// </summary>
+    public partial class RegisterToNewCouse : Window
+    {
+        public RegisterToNewCouse()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+            Student s = new Student();
+            foreach (var x in MainWindow.AllCoursesDictionary)
+            {
+                if ((s.CheckPrequired(x.Key) == true) && (x.Value.CurrentNumberOfStudents < x.Value.MaximumNumberOfStudents))
+                {
+
+                    AvailableCourseBox.Items.Add(x.Key);
+
+
+                }
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Course c = new Course();
+            MainWindow.AllStudentsDictionary[StudentLogIn.StudentID].CoursesInProgress.Add(c);
+            MainWindow.AllCoursesDictionary[c.CourseName].CurrentNumberOfStudents++;
+        }
+    }
+}
+
