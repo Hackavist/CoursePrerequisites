bd83798254614fd99185aa2d9d8b8119ff72720e
hazemkhairy <hazemkhairy188@gmail.com>
2017-12-20 16:11:38 +0200

    edited View courses of specific student

--- Course Prerequsites WPF/UIs/ViewAllCoursesOfStudent.xaml.cs
+++ Course Prerequsites WPF/UIs/ViewAllCoursesOfStudent.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using Course_Prerequsites_WPF.Classes;
 namespace Course_Prerequsites_WPF.UIs
 {
     /// <summary>
@@ -23,51 +23,82 @@ namespace Course_Prerequsites_WPF.UIs
         {
             InitializeComponent();
         }
+        List<string> InProgress;
+        List<string> Finished;
         private void View_button_Click(object sender, RoutedEventArgs e)
         {
-            // clear the list if there were exist 
+                // clear the list if there were exist 
             ListOfCompletedCoursesOfStudent.Items.Clear();
             ListOfProgressCoursesOfStudent.Items.Clear();
-            if (SelectStudentComboBox.SelectedItem == null)
+
+            if (SelectStudentComboBox.SelectedIndex != -1)
             {
-                MessageBox.Show("please select a student to view his courses");
+
+                string[] tosperate = SelectStudentComboBox.SelectedItem.ToString().Split(' ');
+                string SelectedId= tosperate[0];
+                InProgress = Admin.GetInProgressCoursesOfSpecificStudent(SelectedId);
+                Finished = Admin.GetAllFinsishedCoursesOfSpecificStudent(SelectedId);
+                foreach(var x in InProgress)
+                {
+                    ListOfProgressCoursesOfStudent.Items.Add(x);
+                }
+                foreach(var x in Finished)
+                {
+                    ListOfCompletedCoursesOfStudent.Items.Add(x);
+                }
             }
             else
-            {
-                string selectedstudent = SelectStudentComboBox.SelectedValue.ToString();
-                if (WelcomePage.AllStudentsDictionary[selectedstudent].CoursesInProgress.Count == 0)
-                {
-                    ListOfProgressCoursesOfStudent.Items.Add("This student have no courses in progress");
-                }
-                else
-                {
-
-                    foreach (var x in WelcomePage.AllStudentsDictionary[selectedstudent].CoursesInProgress)
-                    {
-                        ListOfProgressCoursesOfStudent.Items.Add(x.CourseName);
-                    }
-                }
-                
-                if (WelcomePage.AllStudentsDictionary[selectedstudent].FinishedCourses.Count == 0)
-                {
-                    ListOfCompletedCoursesOfStudent.Items.Add("This student have no finished courses");
-                }
-                else
-                {
-
-                    foreach (var x in WelcomePage.AllStudentsDictionary[selectedstudent].FinishedCourses)
-                    {
-                        ListOfCompletedCoursesOfStudent.Items.Add(x.CourseName);
-                    }
-                }
-            }
+                MessageBox.Show("Please select student to view his courses ");
+            
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             foreach (var x in WelcomePage.AllStudentsDictionary)
             {
-                SelectStudentComboBox.Items.Add(x.Key);
+                SelectStudentComboBox.Items.Add(x.Key+' ' + x.Value.Name);
             }
         }
+        void DisplayCourseDetails(string coursename)
+        {
+            Course tmp = WelcomePage.AllCoursesDictionary[coursename];
+            InfoBlock.Visibility = System.Windows.Visibility.Visible;
+            NameTextBlock.Text = "Course Name : " + tmp.CourseName;
+            CodeTextBlock.Text = "Course Code : " + tmp.Code;
+            CurrentstudentTextBlock.Text = "Current Number of Students : " + tmp.CurrentNumberOfStudents;
+            MaximumStudentTextBlock.Text = "Maximum Number of Students : " + tmp.MaximumNumberOfStudents;
+            PassingGradeTextBlock.Text = "Passing Grade : " + tmp.PassingGrade.ToString() ;
+            MaxGradeTextBlock.Text = "Total Grade : " + tmp.CourseGrade.ToString();
+            HoursTextBlock.Text = "Total Hours : " + tmp.Hours.ToString();
+            InstructorTextBlock.Text = "Instructor Name : " + tmp.Instructor;
+            DescriptionTextBlock.Text = "Description : " + tmp.Description;
+        }
+
+        private void ListOfProgressCoursesOfStudent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListOfProgressCoursesOfStudent.SelectedIndex != -1)
+            {
+                DisplayCourseDetails(ListOfProgressCoursesOfStudent.SelectedValue.ToString());
+            }
+            else
+            {
+                InfoBlock.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void ListOfCompletedCoursesOfStudent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            if (ListOfCompletedCoursesOfStudent.SelectedIndex!=-1)
+            {
+                DisplayCourseDetails(ListOfCompletedCoursesOfStudent.SelectedValue.ToString());
+            }
+            else
+            {
+                InfoBlock.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+       
+
     }
 }
