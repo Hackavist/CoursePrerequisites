418477afff14ec4d52a17da16b2887b35329f579
hazemkhairy <hazemkhairy188@gmail.com>
2017-12-23 14:45:07 +0200

    improved enter the student grade

--- Course Prerequsites WPF/UIs/EnterStudentGrades.xaml.cs
+++ Course Prerequsites WPF/UIs/EnterStudentGrades.xaml.cs
@@ -31,6 +31,7 @@ namespace Course_Prerequsites_WPF.UIs
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+
             foreach (var x in WelcomePage.AllStudentsDictionary)
             {
                 StudentsComboBox.Items.Add(x.Key+' ' +x.Value.Name);
@@ -63,10 +64,10 @@ namespace Course_Prerequsites_WPF.UIs
         }
         bool ValidMark(string mark)
         {
-            //check if the text have some text other than numbers and '.' 
+            //check if the text have some text other than numbers 
             for (int i = 0 ; i < mark.Length ; i++)
             {
-                if ((mark[i]<'0'||mark[i]>'9')&&mark[i]!='.')
+                if ((mark[i]<'0'||mark[i]>'9'))
                 {
                     return false;
                 }
@@ -84,15 +85,33 @@ namespace Course_Prerequsites_WPF.UIs
         void EnterGrades()
         {
             int PassedCourses = 0;
+            List<Course_Prerequsites_WPF.Classes.Course> finishedtmp = new List<Classes.Course>();
             foreach (var x in CoursesGradeTextBox)
             {
-                if (WelcomePage.AllCoursesDictionary[x.Tag.ToString()].CourseGrade<= double.Parse( x.Text) )
+                if (WelcomePage.AllCoursesDictionary[x.Tag.ToString()].PassingGrade <= int.Parse(x.Text) && WelcomePage.AllCoursesDictionary[x.Tag.ToString()].CourseGrade>=int.Parse(x.Text)) 
                 {
                     // if the student pass this course add this course to his finished courses
-                    WelcomePage.AllStudentsDictionary[SelectedStudent].FinishedCourses.Add( WelcomePage.AllCoursesDictionary[x.Tag.ToString()] );
+                    finishedtmp.Add(WelcomePage.AllCoursesDictionary[x.Tag.ToString()]);
                     PassedCourses++;
                 }
-                //WelcomePage.AllStudentsDictionary[SelectedStudent].CoursesInProgress.Remove(WelcomePage.AllCoursesDictionary[x.Name]);
+                else if (WelcomePage.AllCoursesDictionary[x.Tag.ToString()].CourseGrade < int.Parse(x.Text))
+                {
+                    //return if the entered grade is greater than the course grade
+                    MessageBox.Show("you entered Integer bigger than the course grade at " + x.Tag.ToString() + " Text box ");
+                    return ;
+                }
+            }
+
+            foreach(var x in finishedtmp)
+            {
+                //add the courses which student passed in to his finished courses    
+                WelcomePage.AllStudentsDictionary[SelectedStudent].FinishedCourses.Add(x);
+            }
+            foreach (var x in WelcomePage.AllStudentsDictionary[SelectedStudent].CoursesInProgress)
+            {
+                
+            // remove the student from the CurrentNumberOfstudent of each course
+                WelcomePage.AllCoursesDictionary[x.CourseName].CurrentNumberOfStudents--;
             }
             //remove all in progress courses of the student
             WelcomePage.AllStudentsDictionary[SelectedStudent].CoursesInProgress = new List<Classes.Course>();
@@ -113,13 +132,13 @@ namespace Course_Prerequsites_WPF.UIs
                 MyGrid.Children.Clear();
                 CoursesGradeTextBox.Clear();
                 CoursesNameLabels.Clear();
-
+                //check if the student has in progress courses
                 if (WelcomePage.AllStudentsDictionary[SelectedStudent].CoursesInProgress.Count == 0)
                 {
                     MessageBox.Show("this student have no Current courses in progress");
                     return;
                 }
-
+                // create text block control in the top
                 int i = 0;
                 TextBlock InfoTmp = new TextBlock();
                 InfoTmp.Margin = new Thickness(200, 10, 0, 0);
@@ -127,11 +146,11 @@ namespace Course_Prerequsites_WPF.UIs
                 MyGrid.Children.Add(InfoTmp);
                 foreach (var x in WelcomePage.AllStudentsDictionary[SelectedStudent].CoursesInProgress)
                 {
-
-                    TextBlock LabelTmp = new TextBlock();
-                    LabelTmp.Text = x.CourseName + " : ";
-                    LabelTmp.Margin = new Thickness(95, 111 + (i * 50), 0, 0);
-
+                    // create a text block holding the name of course
+                    TextBlock TextBlockTmp = new TextBlock();
+                    TextBlockTmp.Text = x.CourseName + " : ";
+                    TextBlockTmp.Margin = new Thickness(95, 111 + (i * 50), 0, 0);
+                    // creating textbox to enter the grade at in and the tag of it has the course name
                     TextBox TextBoxTmp = new TextBox();
                     TextBoxTmp.VerticalAlignment = System.Windows.VerticalAlignment.Top;
                     TextBoxTmp.Width = 120;
@@ -139,14 +158,15 @@ namespace Course_Prerequsites_WPF.UIs
                     TextBoxTmp.ToolTip = "Enter the Grade of " + x.CourseName;
                     TextBoxTmp.Margin = new Thickness(371, 111 + (i * 50), 0, 0);
                     TextBoxTmp.Tag = x.CourseName.ToString();
-
-                    MyGrid.Children.Add(LabelTmp);
+                    //add the controls to the grid
+                    MyGrid.Children.Add(TextBlockTmp);
                     MyGrid.Children.Add(TextBoxTmp);
 
                     i++;
-                    CoursesNameLabels.Add(LabelTmp);
+                    CoursesNameLabels.Add(TextBlockTmp);
                     CoursesGradeTextBox.Add(TextBoxTmp);
                 }
+                //add Done Button at the end of grid
                 AddGrades.Content = "Done";
 
                 AddGrades.Height = 30;
